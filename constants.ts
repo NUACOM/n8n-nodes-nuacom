765d7c3e9d6068b2b50657e671847fc96d4ae1d0
eduardohf14 <eduardohf14@hotmail.com>
2026-05-19 11:03:31 -0300

    fix: switch base URL to production

--- constants.ts
+++ constants.ts
@@ -1,2 +1,1 @@
-// export const NUACOM_BASE_URL = 'https://api.api-nuacom.com';
-export const NUACOM_BASE_URL = 'https://dev-env-api.nuacom.ie';
+export const NUACOM_BASE_URL = 'https://api.api-nuacom.com';
